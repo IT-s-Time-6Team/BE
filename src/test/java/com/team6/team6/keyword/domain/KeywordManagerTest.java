47a96d7f7e19e5c6f69cef07b231edef9f5022c4
WonyuChoi <phillip07290@naver.com>
2025-04-25 23:13:59 +0900

    test: 키워드 매니저 최종 변환 테스트

--- src/test/java/com/team6/team6/keyword/domain/KeywordManagerTest.java
+++ src/test/java/com/team6/team6/keyword/domain/KeywordManagerTest.java
@@ -1,5 +1,6 @@
 package com.team6.team6.keyword.domain;
 
+import com.team6.team6.keyword.dto.AnalysisResult;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -8,6 +9,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -42,5 +44,30 @@ class KeywordManagerTest {
         verify(store).saveKeyword(roomId, keyword);
         verify(store).getKeywords(roomId);
         verify(analyser).analyse(keywordsInStore);
+    }
+
+    @Test
+    void keyword_manager_최종_변환_테스트() {
+        // given
+        Long roomId = 1L;
+        String keyword = "AI";
+
+        List<String> keywordsInStore = List.of("AI", "Deep Learning");
+        List<List<String>> expectedResult = List.of(keywordsInStore);
+
+        doNothing().when(store).saveKeyword(roomId, keyword);
+        when(store.getKeywords(roomId)).thenReturn(keywordsInStore);
+        when(analyser.analyse(keywordsInStore)).thenReturn(expectedResult);
+
+        // when
+        List<AnalysisResult> results = keywordManager.addKeyword(roomId, keyword);
+
+        // then
+        assertSoftly(softly -> {
+            softly.assertThat(results).hasSize(1);
+            softly.assertThat(results.get(0).referenceName()).isEqualTo("AI");
+            softly.assertThat(results.get(0).count()).isEqualTo(2);
+            softly.assertThat(results.get(0).variations()).containsExactly("AI", "Deep Learning");
+        });
     }
 }
