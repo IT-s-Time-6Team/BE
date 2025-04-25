0aab3df994a201269155f76d08b455c99030aba3
WonyuChoi <phillip07290@naver.com>
2025-04-25 23:10:54 +0900

    feat: 키워드 매니저 내부 협력 관계 테스트

---
+++ src/test/java/com/team6/team6/keyword/domain/KeywordManagerTest.java
@@ -0,0 +1,46 @@
+package com.team6.team6.keyword.domain;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class KeywordManagerTest {
+
+    @Mock
+    private KeywordStore store;
+
+    @Mock
+    private KeywordSimilarityAnalyser analyser;
+
+    @InjectMocks
+    private KeywordManager keywordManager;
+
+    @Test
+    void 저장소와_분석기_협력_관계_테스트() {
+        // given
+        Long roomId = 1L;
+        String keyword = "AI";
+
+        List<String> keywordsInStore = List.of("AI", "Deep Learning");
+        List<List<String>> expectedResult = List.of(keywordsInStore);
+
+        doNothing().when(store).saveKeyword(roomId, keyword);
+        when(store.getKeywords(roomId)).thenReturn(keywordsInStore);
+        when(analyser.analyse(keywordsInStore)).thenReturn(expectedResult);
+
+        // when
+        keywordManager.addKeyword(roomId, keyword);
+
+        // then
+        verify(store).saveKeyword(roomId, keyword);
+        verify(store).getKeywords(roomId);
+        verify(analyser).analyse(keywordsInStore);
+    }
+}
