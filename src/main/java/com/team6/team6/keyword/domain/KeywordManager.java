a7089736d6d7dcfb4380adb1ba7dc2d10a3308ef
WonyuChoi <phillip07290@naver.com>
2025-04-25 23:12:13 +0900

    feat: 키워드 매니저 내부 협력 관계 구성

--- src/main/java/com/team6/team6/keyword/domain/KeywordManager.java
+++ src/main/java/com/team6/team6/keyword/domain/KeywordManager.java
@@ -1,14 +1,24 @@
 package com.team6.team6.keyword.domain;
 
 import com.team6.team6.keyword.dto.AnalysisResult;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
+@RequiredArgsConstructor
 public class KeywordManager {
 
+    private final KeywordStore keywordStore;
+    private final KeywordSimilarityAnalyser keywordSimilarityAnalyser;
+
     public List<AnalysisResult> addKeyword(Long roomId, String keyword) {
+
+        keywordStore.saveKeyword(roomId, keyword);
+        List<String> keywordsInStore = keywordStore.getKeywords(roomId);
+        List<List<String>> groupedResult = keywordSimilarityAnalyser.analyse(keywordsInStore);
+
         return List.of(AnalysisResult.of("Keyword1", List.of("variation1", "variation2")),
                 AnalysisResult.of("Keyword2", List.of("variation3", "variation4")));
     }
