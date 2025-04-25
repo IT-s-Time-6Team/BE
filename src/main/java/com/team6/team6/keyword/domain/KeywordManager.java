12a22e54313bd0bd463028f0c608a594288e9363
WonyuChoi <phillip07290@naver.com>
2025-04-25 23:28:20 +0900

    feat: 분석기에서 빈 그룹 반환시 해당 그룹 제외

--- src/main/java/com/team6/team6/keyword/domain/KeywordManager.java
+++ src/main/java/com/team6/team6/keyword/domain/KeywordManager.java
@@ -25,11 +25,12 @@ public class KeywordManager {
 
     private List<AnalysisResult> convertToAnalysisResult(List<List<String>> groupedResult, List<String> keywordsInStore) {
         return groupedResult.stream()
+                .filter(group -> !group.isEmpty()) // 빈 그룹 제외
                 .map(group -> {
                     // 그룹에서 keywordsInStore에 가장 먼저 등장한 키워드를 찾기
                     String referenceName = group.stream()
                             .min(Comparator.comparingInt(keywordsInStore::indexOf))
-                            .orElseThrow(); // 빈 그룹은 없다고 가정
+                            .get();
                     return AnalysisResult.of(referenceName, group);
                 })
                 .toList();
