0f0c23e1a5a15541b080312936cd4c99b9dd1ba9
dongsuk <scienceyds@naver.com>
2025-04-29 02:06:42 +0900

    feat: RoomService 클래스 및 방 생성, 조회, 종료 로직 추가

--- src/main/java/com/team6/team6/Team6Application.java
+++ src/main/java/com/team6/team6/Team6Application.java
@@ -2,7 +2,9 @@ package com.team6.team6;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
+@EnableRetry
 @SpringBootApplication
 public class Team6Application {
 
