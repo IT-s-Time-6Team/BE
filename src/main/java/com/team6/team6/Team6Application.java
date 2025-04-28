00513edaf34837fde59ca766420e66edf5e396dc
dongsuk <scienceyds@naver.com>
2025-04-29 06:09:11 +0900

    feat: JPA Auditing 활성화 추가

--- src/main/java/com/team6/team6/Team6Application.java
+++ src/main/java/com/team6/team6/Team6Application.java
@@ -2,9 +2,11 @@ package com.team6.team6;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.retry.annotation.EnableRetry;
 
 @EnableRetry
+@EnableJpaAuditing
 @SpringBootApplication
 public class Team6Application {
 
