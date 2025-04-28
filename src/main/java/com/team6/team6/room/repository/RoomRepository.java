a8bdccfd15e2bdeac99982a9397940206103b8cf
dongsuk <scienceyds@naver.com>
2025-04-28 22:59:36 +0900

    feat: Room 저장소 인터페이스 추가

---
+++ src/main/java/com/team6/team6/room/repository/RoomRepository.java
@@ -0,0 +1,8 @@
+package com.team6.team6.room.repository;
+
+import com.team6.team6.room.entity.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+interface RoomRepository extends JpaRepository<Room, Long> {
+
+}
