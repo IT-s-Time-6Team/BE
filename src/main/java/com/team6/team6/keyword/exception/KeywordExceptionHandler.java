74e9ec7ead40e0e52403ba7a552e567c99949214
WonyuChoi <phillip07290@naver.com>
2025-04-28 22:20:32 +0900

    feat: 커스텀 예외 처리

--- src/main/java/com/team6/team6/keyword/exception/KeywordExceptionHandler.java
+++ src/main/java/com/team6/team6/keyword/exception/KeywordExceptionHandler.java
@@ -1,11 +1,29 @@
 package com.team6.team6.keyword.exception;
 
+import com.team6.team6.keyword.exception.exceptions.EmptyKeywordException;
+import com.team6.team6.keyword.exception.exceptions.InvalidAiResponseException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class KeywordExceptionHandler {
+
+    @ExceptionHandler(EmptyKeywordException.class)
+    public ResponseEntity<String> handleEmptyKeywordException(final EmptyKeywordException exception) {
+        return getResponseWithStatus(HttpStatus.BAD_REQUEST, exception);
+    }
+
+    @ExceptionHandler(EmptyKeywordException.class)
+    public ResponseEntity<String> handleEmptyKeywordException(final InvalidAiResponseException exception) {
+        return getResponseWithStatus(HttpStatus.INTERNAL_SERVER_ERROR, exception);
+    }
+
+    @ExceptionHandler(InvalidAiResponseException.class)
+    public ResponseEntity<String> handleInvalidAiResponseException(final InvalidAiResponseException exception) {
+        return getResponseWithStatus(HttpStatus.INTERNAL_SERVER_ERROR, exception);
+    }
 
     private ResponseEntity<String> getResponseWithStatus(final HttpStatus httpStatus,
                                                          final RuntimeException exception) {
