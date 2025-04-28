805188360bd41a796223784cb902bf0aad30e028
dongsuk <scienceyds@naver.com>
2025-04-29 02:06:55 +0900

    feat: 글로벌 예외 처리 핸들러 및 커스텀 예외 클래스 추가

---
+++ src/main/java/com/team6/team6/global/error/exception/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package com.team6.team6.global.error.exception;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
