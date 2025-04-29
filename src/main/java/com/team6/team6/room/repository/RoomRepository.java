95bfb37b243fc30fc64ae2fd58731892a94b503c
dongsuk <scienceyds@naver.com>
2025-04-30 03:12:36 +0900

    feat: 비관적 락을 사용한 findByRoomKeyWithLock 메서드 추가

--- src/main/java/com/team6/team6/room/repository/RoomRepository.java
+++ src/main/java/com/team6/team6/room/repository/RoomRepository.java
@@ -1,7 +1,11 @@
 package com.team6.team6.room.repository;
 
 import com.team6.team6.room.entity.Room;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,4 +14,8 @@ import java.util.Optional;
 public interface RoomRepository extends JpaRepository<Room, Long> {
 
     Optional<Room> findByRoomKey(String roomKey);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT r FROM Room r WHERE r.roomKey = :roomKey")
+    Optional<Room> findByRoomKeyWithLock(@Param("roomKey") String roomKey);
 }
