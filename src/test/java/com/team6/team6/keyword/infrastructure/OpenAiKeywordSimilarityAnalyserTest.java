b1fa0f46c488e76f028596a31ef348892b2668f2
WonyuChoi <phillip07290@naver.com>
2025-04-29 15:11:38 +0900

    test: SpringBootTest 비활성화

--- src/test/java/com/team6/team6/keyword/infrastructure/OpenAiKeywordSimilarityAnalyserTest.java
+++ src/test/java/com/team6/team6/keyword/infrastructure/OpenAiKeywordSimilarityAnalyserTest.java
@@ -8,25 +8,25 @@ import java.util.List;
 
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
-@SpringBootTest
+//@SpringBootTest
 class OpenAiKeywordSimilarityAnalyserTest {
 
-    @Autowired
-    private OpenAiKeywordSimilarityAnalyser openAiKeywordSimilarityAnalyser;
-
-    @Test
-    void 그룹핑_테스트() {
-        // Given
-        List<String> keywords = List.of("롤", "리그오브레전드", "LOL", "리그 오브 레전드");
-
-        // When
-        List<List<String>> result = openAiKeywordSimilarityAnalyser.analyse(keywords);
-
-        // Then
-        assertSoftly(softly -> {
-            softly.assertThat(result).isNotNull();
-            softly.assertThat(result).hasSize(1);
-            softly.assertThat(result.get(0)).containsExactly("롤", "리그오브레전드", "LOL", "리그 오브 레전드");
-        });
-    }
+//    @Autowired
+//    private OpenAiKeywordSimilarityAnalyser openAiKeywordSimilarityAnalyser;
+//
+//    @Test
+//    void 그룹핑_테스트() {
+//        // Given
+//        List<String> keywords = List.of("롤", "리그오브레전드", "LOL", "리그 오브 레전드");
+//
+//        // When
+//        List<List<String>> result = openAiKeywordSimilarityAnalyser.analyse(keywords);
+//
+//        // Then
+//        assertSoftly(softly -> {
+//            softly.assertThat(result).isNotNull();
+//            softly.assertThat(result).hasSize(1);
+//            softly.assertThat(result.get(0)).containsExactly("롤", "리그오브레전드", "LOL", "리그 오브 레전드");
+//        });
+//    }
 }
