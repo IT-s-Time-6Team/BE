2128f90345afa0d2bb210ff2c8b37cba8e1a80bd
WonyuChoi <phillip07290@naver.com>
2025-04-29 22:32:40 +0900

    feat: 특정 키워드의 모든 질문을 가져오는 메서드 추가

--- src/main/java/com/team6/team6/question/domain/QuestionRepository.java
+++ src/main/java/com/team6/team6/question/domain/QuestionRepository.java
@@ -4,7 +4,10 @@ import com.team6.team6.question.entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
     boolean existsByKeyword(String keyword);
+    List<Question> findAllByKeyword(String keyword);
 }
