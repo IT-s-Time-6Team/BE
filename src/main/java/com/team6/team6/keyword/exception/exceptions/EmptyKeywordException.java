86a3bc191edaaaaffc2fb70a6496292de893eaac
WonyuChoi <phillip07290@naver.com>
2025-04-30 23:08:44 +0900

    feat: 필요 없는 클래스 삭제

--- src/main/java/com/team6/team6/keyword/exception/exceptions/EmptyKeywordException.java
+++
@@ -1,8 +0,0 @@
-package com.team6.team6.keyword.exception.exceptions;
-
-public class EmptyKeywordException extends RuntimeException {
-
-    public EmptyKeywordException() {
-        super("분석할 키워드가 없습니다.");
-    }
-}
