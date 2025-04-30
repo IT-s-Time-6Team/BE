1e492a7679b5b2039d6dd3a8914e622825a7d26a
dongsuk <scienceyds@naver.com>
2025-05-01 02:39:24 +0900

    refactor: Long roomId -> String roomKey 로직 수정

--- src/main/java/com/team6/team6/keyword/service/KeywordAsyncProcessor.java
+++ src/main/java/com/team6/team6/keyword/service/KeywordAsyncProcessor.java
@@ -19,7 +19,7 @@ public class KeywordAsyncProcessor {
     private final QuestionService questionService;
 
     @Async
-    public void processKeywordAnalysisAsync(Long roomId, String keyword) {
+    public void processKeywordAnalysisAsync(Long roomId,String roomKey, String keyword) {
         // 키워드 분석 실행
         List<AnalysisResult> results = keywordManager.addKeyword(roomId, keyword);
 
@@ -29,6 +29,6 @@ public class KeywordAsyncProcessor {
         }
 
         // 분석 완료 후 결과 브로드캐스팅
-        messagePublisher.publishKeywordAnalysisResult(roomId, results);
+        messagePublisher.publishKeywordAnalysisResult(roomKey, results);
     }
 }
