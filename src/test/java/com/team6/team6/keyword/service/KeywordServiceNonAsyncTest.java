6a506d9eac65c27a54cc79e9a4c5b0ab5359e17f
WonyuChoi <phillip07290@naver.com>
2025-05-02 21:04:49 +0900

    test: pool size 설정 파일로 분리, 프로필 활성화

--- src/test/java/com/team6/team6/keyword/service/KeywordServiceNonAsyncTest.java
+++ src/test/java/com/team6/team6/keyword/service/KeywordServiceNonAsyncTest.java
@@ -11,17 +11,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
 @SpringBootTest
-@TestPropertySource(properties = {
-        "spring.task.execution.pool.core-size=4",
-        "spring.task.execution.pool.max-size=8"
-})
+@ActiveProfiles("test")
 class KeywordServiceNonAsyncTest {
 
     @TestConfiguration
