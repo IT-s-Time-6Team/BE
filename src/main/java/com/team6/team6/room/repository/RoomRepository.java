4106e945759b4b8063c8c355ba82735c0d61d63c
WonyuChoi <phillip07290@naver.com>
2025-05-06 21:46:37 +0900

    feat: 가장 많은 키워드를 생성한 멤버와 공감 키워드들과 가장 많이 겹친 멤버 리스트를 가져오는 메서드 추가

--- src/main/java/com/team6/team6/room/repository/RoomRepository.java
+++ src/main/java/com/team6/team6/room/repository/RoomRepository.java
@@ -1,8 +1,10 @@
 package com.team6.team6.room.repository;
 
+import com.team6.team6.room.dto.MemberKeywordCount;
 import com.team6.team6.room.entity.Room;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -13,4 +15,8 @@ public interface RoomRepository {
     Optional<Room> findByRoomKeyWithLock(String roomKey);
 
     Room save(Room room);
+
+    List<MemberKeywordCount> findMembersWithMostKeywordsInRoom(String roomKey);
+
+    List<MemberKeywordCount> findMembersWithMostSharedKeywordsInRoom(String roomKey, List<String> sharedKeywords);
 }
