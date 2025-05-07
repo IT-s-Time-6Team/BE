01271a71e09bc5bd1afbebe179c817b816d1191b
dongsuk <scienceyds@naver.com>
2025-05-08 03:42:17 +0900

    feat: Redis 설정 클래스 추가

---
+++ src/main/java/com/team6/team6/global/config/RedisConfig.java
@@ -0,0 +1,30 @@
+package com.team6.team6.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    // 이벤트 리스너를 등록하기 위한 RedisMessageListenerContainer 빈을 생성합니다.
+    @Bean
+    public RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory connectionFactory) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        return container;
+    }
+
+    // RedisTemplate을 생성하여 Redis와의 데이터 상호작용을 위한 템플릿을 제공합니다.
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
+}
