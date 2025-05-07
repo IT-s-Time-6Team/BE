eb4a2ef9e7029957e93627f144d1259b22b46f2e
dongsuk <scienceyds@naver.com>
2025-05-08 04:12:07 +0900

    refactor: RedisTemplate 관련 불필요한 코드 제거

--- src/main/java/com/team6/team6/global/config/RedisConfig.java
+++ src/main/java/com/team6/team6/global/config/RedisConfig.java
@@ -3,9 +3,7 @@ package com.team6.team6.global.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
@@ -18,13 +16,4 @@ public class RedisConfig {
         return container;
     }
 
-    // RedisTemplate을 생성하여 Redis와의 데이터 상호작용을 위한 템플릿을 제공합니다.
-    @Bean
-    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(connectionFactory);
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
-        return redisTemplate;
-    }
 }
