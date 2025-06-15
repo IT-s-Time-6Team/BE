02dc25844776dea1e848b6e0cd78d46bafe6b861
WonyuChoi <phillip07290@naver.com>
2025-06-15 18:40:54 +0900

    feat: AnalysisResults 객체를 Redis에 저장하기 위한 RedisTemplate Bean 추가

--- src/main/java/com/team6/team6/global/config/RedisConfig.java
+++ src/main/java/com/team6/team6/global/config/RedisConfig.java
@@ -1,11 +1,12 @@
 package com.team6.team6.global.config;
 
+import com.team6.team6.keyword.dto.AnalysisResults;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -19,15 +20,15 @@ public class RedisConfig {
         return container;
     }
 
+    // AnalysisResults 객체를 Redis에 저장하기 위한 RedisTemplate을 생성합니다.
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
+    public RedisTemplate<String, AnalysisResults> analysisResultsRedisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, AnalysisResults> template = new RedisTemplate<>();
+        Jackson2JsonRedisSerializer<AnalysisResults> serializer =
+                new Jackson2JsonRedisSerializer<>(AnalysisResults.class);
         template.setConnectionFactory(connectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-        template.setHashKeySerializer(new StringRedisSerializer());
-        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
-        template.afterPropertiesSet();
+        template.setValueSerializer(serializer);
         return template;
     }
 }
